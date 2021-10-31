ab6a26100a7195c864825daa67b82fd1a3185947
Farhad Deghat <deghat.farhad@gmail.com>
2021-10-31 16:30:49 +0330

    map view initialization.

--- car_list/src/main/java/deghat/farhad/carlist/presentation/page/placemark_map/view/FragPlacemarkMap.kt
+++ car_list/src/main/java/deghat/farhad/carlist/presentation/page/placemark_map/view/FragPlacemarkMap.kt
@@ -1,28 +1,41 @@
 package deghat.farhad.carlist.presentation.page.placemark_map.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 import dagger.hilt.android.AndroidEntryPoint
 import deghat.farhad.carlist.BR
 import deghat.farhad.carlist.R
-import deghat.farhad.carlist.presentation.page.placemark_list.viewmodel.ViwMdlPlacemarkList
 import deghat.farhad.carlist.presentation.page.placemark_map.viewmodel.ViwMdlPlacemarkMap
 
 @AndroidEntryPoint
-class FragPlacemarkMap : Fragment() {
+class FragPlacemarkMap : Fragment(), OnMapReadyCallback {
+    companion object {
+        const val PERMISSIONS_REQUEST_LOCATION = 101
+    }
+
 
     private val viewModel: ViwMdlPlacemarkMap by viewModels()
+
+    lateinit var googleMap: GoogleMap
+    lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setObservers()
         return setDataBinding(inflater, container)
     }
 
@@ -36,5 +49,62 @@ class FragPlacemarkMap : Fragment() {
             setVariable(BR.viewmodel, viewModel)
             lifecycleOwner = this@FragPlacemarkMap
         }.root
+    }
+
+    fun setObservers() {
+        viewModel.isPermissionGranted.observe(viewLifecycleOwner) {
+            mapView.getMapAsync(this)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView(savedInstanceState)
+    }
+
+    private fun initView(savedInstanceState: Bundle?) {
+        mapView = requireActivity().findViewById(R.id.map)
+        mapView.onCreate(savedInstanceState)
+        requestPermissions(
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSIONS_REQUEST_LOCATION
+        )
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                viewModel.setPermissionGranted(true)
+            } else {
+                viewModel.setPermissionGranted(false)
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            this.googleMap.isMyLocationEnabled = true
+        }
     }
 }
--- car_list/src/main/java/deghat/farhad/carlist/presentation/page/placemark_map/viewmodel/ViwMdlPlacemarkMap.kt
+++ car_list/src/main/java/deghat/farhad/carlist/presentation/page/placemark_map/viewmodel/ViwMdlPlacemarkMap.kt
@@ -1,9 +1,14 @@
 package deghat.farhad.carlist.presentation.page.placemark_map.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ViwMdlPlacemarkMap @Inject constructor(): ViewModel() {
+    val isPermissionGranted by lazy { MutableLiveData<Boolean>() }
+    fun setPermissionGranted(value: Boolean) {
+        isPermissionGranted.postValue(value)
+    }
 }
